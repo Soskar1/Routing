09f0f14eb232e54c1536c39905d4685f813aeff6
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-05-10 19:12:18 +0300

    Added network.java router.java

---
+++ src/main/java/network/Network.java
@@ -0,0 +1,35 @@
+package network;
+
+import graphs.WeightedGraph;
+
+import java.util.ArrayList;
+
+public class Network {
+    private final WeightedGraph<Router> graph;
+    private final ArrayList<Router> routers;
+
+    public Network() {
+        graph = new WeightedGraph<>();
+        routers = new ArrayList<>();
+    }
+
+    public void addRouter(Router router) {
+        if (!routers.contains(router)) {
+            graph.addNode(router.hashCode(), router);
+            routers.add(router);
+        }
+    }
+
+    public void removeRouter(Router router) {
+        if (routers.contains(router)) {
+            graph.removeNode(router.hashCode());
+            routers.remove(router);
+        }
+    }
+
+    public void connectRouters(Router firstRouter, Router secondRouter, int connectionCost) {
+        if (routers.contains(firstRouter) && routers.contains(secondRouter)) {
+            graph.connectNodes(firstRouter.hashCode(), secondRouter.hashCode(), connectionCost);
+        }
+    }
+}
